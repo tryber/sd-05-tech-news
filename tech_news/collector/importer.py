9f3be056c45b15ab1b8709ccb448099efa085bd3
Jorge Lucas <jorge.lucas2264@gmail.com>
2021-04-05 16:02:10 -0300

    requisito 3 completo

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,17 @@
+import csv
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    try:
+        assert filepath.endswith('.csv')
+        with open(filepath) as file:
+            filepath_reader = csv.reader(file, delimiter=";")
+            header, *data = filepath_reader
+    except AssertionError:
+        raise ValueError("Formato invalido")
+    except FileNotFoundError:
+        raise ValueError(f'Arquivo {filepath} não encontrado')
+    else:
+        newArr = []
+        for item in data:
+            newArr.append(dict(zip(header, item)))
+        return newArr
