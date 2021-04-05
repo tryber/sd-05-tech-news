5781f3ba472fbef242b86b1224381d99e6b42b5d
llsxavier <leonardoxw@gmail.com>
2021-04-05 08:18:09 -0300

    testando se teste passa com alerta de complexidade

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,8 +1,15 @@
+import sys
 from tech_news.collector.importer import csv_importer
 from tech_news.collector.exporter import csv_exporter
 from tech_news.collector.scrapper import scrape, fetch_content
 from tech_news.database import create_news
-import sys
+from tech_news.analyzer.ratings import top_5_categories, top_5_news
+from tech_news.analyzer.search_engine import (
+    search_by_category,
+    search_by_date,
+    search_by_source,
+    search_by_title,
+)
 
 
 def collector_menu():
@@ -34,3 +41,41 @@ def collector_menu():
 
 def analyzer_menu():
     """Seu código deve vir aqui"""
+    option = input(
+        "Selecione uma das opções a seguir:\n"
+        " 1 - Buscar notícias por título;\n"
+        " 2 - Buscar notícias por data;\n"
+        " 3 - Buscar notícias por fonte;\n"
+        " 4 - Buscar notícias por categoria;\n"
+        " 5 - Listar top 5 notícias;\n"
+        " 6 - Listar top 5 categorias;\n"
+        " 7 - Sair.\n"
+    )
+    if option == "1":
+        result = input("Digite o título:")
+        title = search_by_title(result)
+        print(title)
+    elif option == "2":
+        result = input("Digite a data no formato aaaa-mm-dd:")
+        date = search_by_date(result)
+        print(date)
+    elif option == "3":
+        result = input("Digite a fonte:")
+        source = search_by_source(result)
+        print(source)
+    elif option == "4":
+        result = input("Digite a categoria:")
+        category = search_by_category(result)
+        print(category)
+    elif option == "5":
+        result = top_5_news()
+        print(result)
+    elif option == "6":
+        result = top_5_categories()
+        print(result)
+    elif option == "7":
+        print("Encerrando script\n")
+        return
+    else:
+        print("Opção inválida", file=sys.stderr)
+        return
