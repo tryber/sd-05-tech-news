b7af64703f96ebc211d8bb0b7807d89a07f733a9
Larissa Palombo <larissa.palombo@tech4h.com.br>
2021-04-08 15:08:46 -0300

    req 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,14 @@
+import requests
+from time import sleep
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=timeout)
+        sleep(delay)
+    except requests.ReadTimeout:
+        return ""
+    else:
+        return response.text
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
