2207483f9e5f6f37ccbfc5f9db1a0305fb2df833
Larissa Palombo <larissa.palombo@tech4h.com.br>
2021-04-16 10:17:08 -0300

    erros

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -2,28 +2,37 @@ import pymongo
 from tech_news.database import db
 from bson.son import SON
 
+
 def top_5_news():
     top_five = []
-    news = (db.news.find({}).sort([
-        ('shares_count', pymongo.DESCENDING),
-        ('comments_count', pymongo.DESCENDING),
-        ('title', pymongo.ASCENDING)
-    ]).limit(5))
+    news = (
+        db.news.find({})
+        .sort(
+            [
+                ("shares_count", pymongo.DESCENDING),
+                ("comments_count", pymongo.DESCENDING),
+                ("title", pymongo.ASCENDING),
+            ]
+        )
+        .limit(5)
+    )
 
     for one in news:
-        top_five.append((one['title'], one['url']))
-   
+        top_five.append((one["title"], one["url"]))
+
     return top_five
 
 
 def top_5_categories():
     top_five = []
-    cat = db.news.aggregate([
-        {"$unwind": "$categories"},
-        {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
-        {"$sort": SON([("count", 1), ("_id", 1)])},
-        {"$limit": 5},
-    ])
+    cat = db.news.aggregate(
+        [
+            {"$unwind": "$categories"},
+            {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+            {"$sort": SON([("count", 1), ("_id", 1)])},
+            {"$limit": 5},
+        ]
+    )
 
     for one in cat:
         top_five.append((one["_id"]))
