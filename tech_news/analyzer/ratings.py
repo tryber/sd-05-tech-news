d49e2372e9a924d9b4c1487f4eb9b1b0b163fb11
samueap <samueap@gmail.com>
2021-04-18 22:55:26 -0300

    req9 and finished

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,28 @@
+
+from tech_news.database import db, client
+
+
 def top_5_news():
     """Seu código deve vir aqui"""
+    result = []
+    search_results = list(
+        db.news.aggregate(
+            [
+                {
+                    "$addFields": {
+                        "count": {"$sum": ["$shares_count", "$comments_count"]}
+                    }
+                },
+                {"$sort": {"count": -1, "title": 1}},
+                {"$limit": 5},
+            ]
+        )
+    )
+    client.close()
+    for news in search_results:
+        result.append((news["title"], news["url"]))
+
+    return result
 
 
 def top_5_categories():
