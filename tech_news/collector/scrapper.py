241a50e4ebbabda82143d427c4097cc9710dc19b
carlanakajuni <carlanakajuni@hotmail.com>
2021-04-26 23:14:55 -0300

    Requisito 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,7 +1,22 @@
 """LETS GET STARTED"""
+import requests
+import time
+
 
 def fetch_content(url, timeout=3, delay=0.5):
     """Seu código deve vir aqui"""
+    try:
+        res = requests.get(url, timeout=timeout)
+        time.sleep(delay)
+
+        if res.status_code != 200:
+            return ""
+
+        return res.text
+
+    except requests.Timeout:
+
+        return ""
 
 
 def scrape(fetcher, pages=1):
