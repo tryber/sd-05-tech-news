e8ec4021650f72f71d2cd640648964918de6a44d
Fernando Luckesi <fernando.luckesi@94gmail.com>
2021-04-27 23:26:21 -0300

    Requisitos 3 e 4 finalizado

--- tech_news/collector/exporter.py
+++ tech_news/collector/exporter.py
@@ -1,2 +1,31 @@
+import csv
+from tech_news import database
+
+
 def csv_exporter(filepath):
-    """Seu código deve vir aqui"""
+
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+    headers = [
+        "url",
+        "title",
+        "timestamp",
+        "writer",
+        "shares_count",
+        "comments_count",
+        "summary",
+        "sources",
+        "categories",
+    ]
+
+    with open(filepath, "w") as file:
+        csv_writer = csv.DictWriter(file, fieldnames=headers, delimiter=";")
+
+        news_db = database.find_news()
+        for new in news_db:
+            for chave in new:
+                if type(new[chave]) == list:
+                    new[chave] = ",".join(new[chave])
+
+        csv_writer.writeheader()
+        csv_writer.writerows(news_db)
--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,14 @@
+import csv
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+    try:
+        with open(filepath, 'r') as file:
+            data = csv.DictReader(file, delimiter=";")
+            for row in data:
+                return [row]
+
+    except FileNotFoundError:
+        raise ValueError(f"Arquivo {filepath} não encontrado")
