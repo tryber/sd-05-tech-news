49e0640d9557ea5ac76ad8fb81df14fcf870ec23
Preto115 <preto115@gmail.com>
2021-05-02 19:12:10 -0300

    requisito 3 implementado

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,9 +1,29 @@
+from tech_news.database import search_news
+from datetime import datetime
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    by_title = search_news({"title": {"$regex": title, "$options": "-i"}})
+    # for news in by_title:
+    if len(by_title) == 0:
+        return []
+    return [(by_title[0]["title"], by_title[0]["url"])]
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    response = []
+    try:
+        date_checked = datetime.strptime(date, "%Y/%m/%d")
+        print(date_checked)
+        by_date = search_news({"timestamp": {"$regex": date_checked}})
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        if len(by_date) == 0:
+            return []
+        for news in by_date:
+            response.append((news["title"], news["url"]))
+        return response
 
 
 def search_by_source(source):
--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -3,38 +3,39 @@ import csv
 
 def csv_importer(filepath):
     """Seu código deve vir aqui"""
-    expected_header = ["url", "title", "timestamp", "writer", "shares_count", "comments_count", "summary", "sources", "categories"]
+    expected_header = [
+        "url", "title", "timestamp", "writer", "shares_count",
+        "comments_count", "summary", "sources", "categories"]
     results = []
-    new = dict()
     try:
-        # print("header: ")
         with open(filepath) as file:
-            # response = csv.DictReader(file, delimiter=";")
             file_readed = csv.reader(file, delimiter=";")
             header, *data = file_readed
-            # for new in response:
-            #     # print("data: ", new)
-            #     results.append(new.items())
-
+            data = data[0]
     except FileNotFoundError:
         if not filepath.endswith("csv"):
             raise ValueError("Formato invalido")
         raise ValueError(f"Arquivo {filepath} não encontrado")
     else:
-        if not filepath.endswith("csv"):
-            raise ValueError("Formato invalido")
-        for idx in expected_header:
-            if idx not in header:
-                raise ValueError("Some header of the file is missing")
-        for idx in range(len(header)):
-            new["url"] = data[0][0]
-            new["title"] = data[0][1]
-            new["timestamp"] = data[0][2]
-            new["writer"] = data[0][3]
-            new["shares_count"] = data[0][4]
-            new["comments_count"] = data[0][5]
-            new["summary"] = data[0][6]
-            new["sources"] = data[0][7]
-            new["categories"] = data[0][8]
-        results.append(new)
+        final_result(filepath, expected_header, header, data, results)
     return results
+
+
+def final_result(filepath, expected_header, header, data, results):
+    if not filepath.endswith("csv"):
+        raise ValueError("Formato invalido")
+    for idx in expected_header:
+        if idx not in header:
+            raise ValueError("Some header of the file is missing")
+    for idx in range(len(header)):
+        new = {}
+        new["url"] = data[0]
+        new["title"] = data[1]
+        new["timestamp"] = data[2]
+        new["writer"] = data[3]
+        new["shares_count"] = data[4]
+        new["comments_count"] = data[5]
+        new["summary"] = data[6]
+        new["sources"] = data[7]
+        new["categories"] = data[8]
+    results.append(new)
