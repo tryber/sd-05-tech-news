13d16d811388010104ce374eccc4da0c6e761c55
Fernando Luckesi <fernando.luckesi@94gmail.com>
2021-05-02 22:27:00 -0300

    60% finalizado

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,14 +1,59 @@
+from tech_news import database
+from datetime import datetime
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    search = database.search_news(
+        {"title": {"$regex": title, "$options": "i"}}
+    )
+    if len(search) == 0:
+        return []
+
+    news = []
+    for new in search:
+        news.append((new["title"], new["url"]))
+    return news
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        search = database.search_news({"timestamp": {"$regex": date}})
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        if len(search) == 0:
+            return []
+
+        news = []
+        for new in search:
+            news.append((new["title"], new["url"]))
+        return news
 
 
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    search = database.search_news(
+        {"sources": {"$regex": source, "$options": "i"}}
+    )
+
+    if len(search) == 0:
+        return []
+
+    news = []
+    for new in search:
+        news.append((new["title"], new["url"]))
+    return news
 
 
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    search = database.search_news(
+        {"categories": {"$regex": category, "$options": "i"}}
+    )
+
+    if len(search) == 0:
+        return []
+
+    news = []
+    for new in search:
+        news.append((new["title"], new["url"]))
+    return news
