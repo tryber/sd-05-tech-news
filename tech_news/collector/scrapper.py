dcad53a802b7ccab3f60bb4c8df868b7dc49adf8
luialbeto <araujo.script@gmail.com>
2021-05-03 22:42:12 -0300

    req 2

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -18,6 +18,16 @@ def fetch_content(url, timeout=3, delay=0.5):
 def scrape(fetcher, pages=1):
     links_all = []
     url_base = "https://www.tecmundo.com.br/novidades"
+    url_dynamic = url_base
+    for page_index in range(pages):
+        array_link = get_links(url_dynamic, fetcher)
+        links_all.extend(array_link)
+        url_dynamic = url_base + "?pages=" + str(page_index + 2)
+    result = []
+    for link_page in links_all:
+        text = fetcher(link_page)
+        result.append(get_news(text, link_page))
+    return result
 
 
 def get_links(url, fetcher):
@@ -25,3 +35,33 @@ def get_links(url, fetcher):
     return selector.css(
         'div.tec--list__item > article > figure > a::attr(href)'
         ).getall()
+
+
+def get_news(text, link_page):
+    selector = Selector(text)
+    return {
+        "url": link_page,
+        "title": selector.css('#js-article-title::text').get().strip(),
+        "timestamp": selector.css(
+            '#js-article-date::attr(datetime)').get().strip(),
+        "writer": selector.css(
+            '.tec--author__info__link::text').get(),
+        "shares_count": int(
+            selector.css(
+                'div.tec--toolbar__item:nth-child(1)::text'
+            ).get().strip().split()[0]
+        ),
+        "comments_count": int(
+            selector.css(
+                '#js-comments-btn::attr(data-count)').get().strip()
+        ),
+        "summary": selector.css(
+            '.tec--article__body > p:nth-child(1)::text'
+            ).get().strip(),
+        "sources": selector.css(
+            'div.z--mb-16 > div > a::text'
+            ).getall(),
+        "categories": selector.css(
+            '#js-categories > a::text'
+            ).getall()
+    }
