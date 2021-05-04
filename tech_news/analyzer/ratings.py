ebd8d2cf683914455a934de7e1fe63489db370c9
Natalia Macedo <nataliasilvamacedo@gmail.com>
2021-05-03 23:53:10 -0300

    Requisito 9

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,19 @@
+from tech_news.database import search_news
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    list_news = search_news({})
+    resultado = sorted(
+        list_news,
+        key=lambda item: item["comments_count"] + item["shares_count"],
+        reverse=True
+        )
+    if len(resultado) == 0:
+        return []
+    return [(item["title"], item["url"]) for item in resultado][:5]
+
+
 
 
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    
