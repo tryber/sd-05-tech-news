a2b747ebcd47fa683ccb2814ce91c90f4f5800e2
Sidnei Ramos <sidneiramos02@gmail.com>
2021-05-04 18:43:35 -0300

    requisito 3

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,16 @@
+import csv
+
+
 def csv_importer(filepath):
     """Seu código deve vir aqui"""
+    if filepath.endswith(".csv") is not True:
+        raise ValueError("Formato invalido")
+    try:
+        with open(filepath) as file:
+            reading = csv.DictReader(file, delimiter=";")
+            archive = []
+            for archive in reading:
+                values = archive
+                return [values]
+    except FileNotFoundError:
+        raise ValueError(f"Arquivo {filepath} não encontrado")
