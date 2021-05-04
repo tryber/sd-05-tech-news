2cf8fc8f333daa203af7b49f486aeb4baf27815b
luialbeto <araujo.script@gmail.com>
2021-05-03 21:27:10 -0300

    req 1

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -1,6 +1,27 @@
+import requests
+from time import sleep
+from parsel import Selector
+
+
 def fetch_content(url, timeout=3, delay=0.5):
-    """Seu código deve vir aqui"""
+    try:
+        resp = requests.get(url, timeout=timeout)
+    except requests.RequestException:
+        return ""
+    else:
+        if resp.status_code != 200:
+            return ""
+        sleep(delay)
+        return resp.text
 
 
 def scrape(fetcher, pages=1):
-    """Seu código deve vir aqui"""
+    links_all = []
+    url_base = "https://www.tecmundo.com.br/novidades"
+
+
+def get_links(url, fetcher):
+    selector = Selector(fetcher(url))
+    return selector.css(
+        'div.tec--list__item > article > figure > a::attr(href)'
+        ).getall()
