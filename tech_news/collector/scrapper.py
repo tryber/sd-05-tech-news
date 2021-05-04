3aa67fad78a2f23cc4ec478d62ffcdfa59f6f02e
Sidnei Ramos <sidneiramos02@gmail.com>
2021-05-03 22:40:19 -0300

    Tentando segundo

--- tech_news/collector/scrapper.py
+++ tech_news/collector/scrapper.py
@@ -20,6 +20,30 @@ def scrape(fetcher, pages=1):
     """Seu código deve vir aqui"""
     links = []
     news_url = "https://www.tecmundo.com.br/novidades"
+    url_dinamic = news_url
+    for page_index in range(pages):
+        links.extend(get_links(url_dinamic, fetcher))
+        url_dinamic = news_url + "?pages=" + str(page_index+2)
+        result = []
+    for unique_link in links:
+        text = fetcher(unique_link)
+        result.append(get_news(text, unique_link))
+    return result
+
+
+def get_news(text, unique_link):
+    selector = Selector(text)
+    return {
+        "url": unique_link,
+        "title": selector.css('#js-article-title::text').get().strip(),
+        "timestamp": selector.css('#js-article-date::attr(datetime)').get().strip(),
+        "writer": selector.css('.tec--author__info__link::text').get().strip(),
+        "shares_count": int(selector.css('.tec--toolbar__item:nth-child(1)::text').get().strip().split()[0]),
+        "comments_count": int(selector.css('#js-comments-btn::attr(data-count)').get().strip()),
+        "summary": selector.css('.tec--article__body > p:nth-child(1)::text').get().strip(),
+        "sources": selector.css('.z--mb-16 > div > a::text').getall(),
+        "categories": selector.css('#js-categories > a::text').getall()
+    }
 
 
 def get_links(url, fetcher):
