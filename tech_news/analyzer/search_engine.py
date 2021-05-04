1822f84271dc5a8d5ca1d9f003248e622648123a
carlanakajuni <carlanakajuni@hotmail.com>
2021-05-03 23:37:17 -0300

    Requisito 5, 6, 7, 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,14 +1,30 @@
+from datetime import datetime
+from tech_news.database import search_news
+
+
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    result = search_news({"title": title.title()})
+    return [(one["title"], one["url"]) for one in result]
 
 
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        result = search_news({"timestamp": {"$regex": date}})
+        return [(one["title"], one["url"]) for one in result]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 def search_by_source(source):
     """Seu código deve vir aqui"""
+    result = search_news({"sources": {"$regex": source, "$options": "i"}})
+    return [(one["title"], one["url"]) for one in result]
 
 
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    result = search_news({"categories": {"$regex": category, "$options": "i"}})
+    return [(one["title"], one["url"]) for one in result]
