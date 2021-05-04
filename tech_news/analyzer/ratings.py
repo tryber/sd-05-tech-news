f3a2e753edbd2652d8a7834824284a7ee9fcc4bc
Preto115 <preto115@gmail.com>
2021-05-03 22:35:49 -0300

    requisito 9 implementado

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,17 @@
+from tech_news.database import filter_top_5_news
+
+
 def top_5_news():
     """Seu código deve vir aqui"""
+    top_5 = filter_top_5_news()
+    topi = []
+    if len(top_5) == 0:
+        return []
+    for top in top_5:
+        topi.append((top["title"], top["url"]))
+    return topi
 
 
 def top_5_categories():
     """Seu código deve vir aqui"""
+#     
