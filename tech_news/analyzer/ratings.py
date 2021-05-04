2ad94b76771ce50d82908441924030d419fb2d36
carlanakajuni <carlanakajuni@hotmail.com>
2021-05-03 23:55:26 -0300

    Requisito 9. Top 5 news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,21 @@
+from tech_news.database import search_news
+
+
 def top_5_news():
     """Seu código deve vir aqui"""
+    news = search_news({})
+    result = sorted(
+        news,
+        key=lambda item: item["comments_count"] + item["shares_count"],
+        reverse=True
+    )
+
+    if len(result) == 0:
+        return []
+
+    listing_top5 = [(item["title"], item["url"]) for item in result][:5]
+
+    return listing_top5
 
 
 def top_5_categories():
