50c7d5b23b4ac5014ed16f48bfea95be68687d48
luialbeto <araujo.script@gmail.com>
2021-05-04 21:40:28 -0300

    req 6

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = search_news({})
+    result = sorted(
+        news,
+        key=lambda item: item["comments_count"] + item["shares_count"],
+        reverse=true,
+    )
+    if len(result) == 0:
+        return []
+    return [(item["title"], item["url"]) for item in result][:5]
 
 
 def top_5_categories():
-    """Seu código deve vir aqui"""
