07e86c773428c91ebcc74218e838798289e4cd50
Sidnei Ramos <sidneiramos02@gmail.com>
2021-05-04 21:40:31 -0300

    metade do 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,18 @@
+from tech_news.database import search_news
+
+
 def top_5_news():
     """Seu código deve vir aqui"""
+    list_news = search_news({})
+    result = sorted(
+        list_news,
+        key=lambda new_count: new_count["comments_count"]
+        + new_count["shares_count"],
+        reverse=True,
+    )
+    if len(result) == 0:
+        return []
+    return [(item["title"], item["url"]) for item in result][:5]
 
 
 def top_5_categories():
