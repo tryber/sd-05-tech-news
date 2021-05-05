f0c0d3b990331e40f7277a6626e5c29ae6e48dcd
Lucas Yoshida <lucalsimayoshida@gmail.com>
2021-05-04 22:21:32 -0300

    requisito 3 ue

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,16 @@
+import csv
+
+
 def csv_importer(filepath):
     """Seu código deve vir aqui"""
+    if filepath.endswith(".csv") is not True:
+        raise ValueError("Formato invalido")
+    try:
+        with open(filepath) as file:
+            csv_reader = csv.DictReader(file, delimiter=";")
+            row = []
+            for row in csv_reader:
+                values = row
+                return [values]
+    except FileNotFoundError:
+        raise ValueError(f"Arquivo {filepath} não encontrado")
