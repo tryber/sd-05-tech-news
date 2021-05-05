f16d8bf3ab77f91126ec7934488476f6cf31d9a8
Lucas Yoshida <lucalsimayoshida@gmail.com>
2021-05-05 11:10:07 -0300

    req 5 e 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,9 +1,23 @@
+from tech_news import database
+from datetime import datetime
+
+
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    news = database.search_news({"title": {"$regex": title, "$options": "i"}})
+    return [(new["title"], new["url"]) for new in news]
 
 
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        news = database.search_news({"title": {"$regex": title, "$options": "i"}})
+    except ValueError:
+        raise ValueError("Data inválida")
+        if len(news) == 0:
+            return []
+    return [(new["title"], new["url"]) for new in news]
 
 
 def search_by_source(source):
