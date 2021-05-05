850e82bfc62ebe9bb8cbb2769a9f2dfce6852b59
Ishidor <kevinmendessilva@hotmail.com>
2021-05-05 15:18:52 -0300

    Online check

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,44 @@
+from tech_news.database import db, client
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    results = []
+    search_results = list(
+        db.news.aggregate(
+            [
+                {
+                    "$addFields": {
+                        "count": {
+                            "$sum": ["$shares_count", "$comments_count"]
+                        }
+                    }
+                },
+                {"$sort": {"count": -1, "title": 1}},
+                {"$limit": 5},
+            ]
+        )
+    )
+    client.close()
+    for news in search_results:
+        results.append((news["title"], news["url"]))
+
+    return results
 
 
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    results = []
+    search_results = list(
+        db.news.aggregate(
+            [
+                {"$unwind": "$categories"},
+                {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+                {"$sort": {"count": -1, "_id": 1}},
+                {"$limit": 5},
+            ]
+        )
+    )
+    client.close()
+    for news in search_results:
+        results.append((news["_id"]))
+
+    return results
--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,15 @@
+import csv
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+    try:
+        with open(filepath) as file:
+            read_content = csv.DictReader(file, delimiter=";")
+            content = ""
+            for content in read_content:
+                values = content
+                return [values]
+    except FileNotFoundError:
+        raise ValueError("Arquivo tests/file_not_exist.csv não encontrado")
