1b645099bf46b54a9051adf59522ae4f02478dc9
Natalia Macedo <nataliasilvamacedo@gmail.com>
2021-05-04 23:04:06 -0300

    Requisito 13 try

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,8 +1,16 @@
 import sys
 from tech_news.collector.importer import csv_importer
 from tech_news.collector.exporter import csv_exporter
-from tech_news.collector.scrapper import scrape, fetch_content 
-from tech_news.database import create_news
+from tech_news.collector.scrapper import scrape, fetch_content
+# from tech_news.database import create_news
+from tech_news.analyzer.search_engine import search_by_title, search_by_date
+from tech_news.analyzer.search_engine import search_by_source
+from tech_news.analyzer.search_engine import search_by_category
+from tech_news.analyzer.ratings import top_5_categories, top_5_news
+
+
+def create_news():
+    pass
 
 
 def collector_menu():
@@ -31,4 +39,28 @@ def collector_menu():
 
 
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    print("Selecione uma das opções a seguir:")
+    print(" 1 - Buscar notícias por título;")
+    print(" 2 - Buscar notícias por data;")
+    print(" 3 - Buscar notícias por fonte;")
+    print(" 4 - Buscar notícias por categoria;")
+    print(" 5 - Listar top 5 notícias;")
+    print(" 6 - Listar top 5 categorias;")
+    print(" 7 - Sair.")
+    options = input()
+    if options == "1":
+        search_by_title(input())
+    elif options == "2":
+        search_by_date(input())
+    elif options == "3":
+        search_by_source(input())
+    elif options == "4":
+        search_by_category(input())
+    elif options == "5":
+        top_5_news(input())
+    elif options == "6":
+        top_5_categories(input())
+    elif options == "7":
+        sys.stdout.write("Encerrando script\n")
+    else:
+        sys.stderr.write("Opção inválida\n")
