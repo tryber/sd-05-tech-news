2621b7a66e1400cc1582f40a60bf6789ed7e05c3
rafaelpbar <rafael.coper@outlook.com>
2021-05-05 15:49:17 -0300

    Req 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -9,6 +9,14 @@ def search_by_title(title):
 
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        news = search_news({"timestamp": {"$regex": date}})
+    except ValueError:
+        raise ValueError("Data inválida")
+    if len(news) == 0:
+        return []
+    return [(item["title"], item["url"]) for item in news]
 
 
 def search_by_source(source):
