761d9a3c5940ae17b87215cdaa089b449a9dc23c
Alexander Mitchell <alexlm.trybe@gmail.com>
2021-05-04 23:54:45 -0300

    nat mandou commitar senao a chinela vai cantar

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,21 @@
+import csv
+
+
 def csv_importer(filepath):
     """Seu código deve vir aqui"""
+
+    if filepath.endswith(".csv") is not True:
+        raise ValueError("Formato invalido")
+    try:
+        with open(filepath) as file:
+            storage = csv.DictReader(file, delimiter=";")
+            content = []
+            for data in storage:
+                content.append(data)
+                return content
+    except FileNotFoundError:
+        raise ValueError(f"Arquivo {filepath} não encontrado")
+
+
+# Testado manualmente com csv_importer("file_csv.csv")
+# file csv já existente no projeto
