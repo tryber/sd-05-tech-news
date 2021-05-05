9dcdfdfe864099ef2adc59d6b5771e409262627a
Maurício <mauricio.sdjr@gmail.com>
2021-05-05 15:24:26 -0300

    requisitos 9 e 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,24 @@
+from tech_news.database import db
+
+""" inspirado no repo do Rafael Leite """
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    aggregation = list(db.news.aggregate([
+        {"$addFields":
+            {"somaSocial": {"$add": ["$comments_count", "$shares_count"]}}},
+        {"$sort": {"somaSocial": -1, "title": 1}},
+        {"$limit": 5},
+        ]))
+    return [(item["title"], item["url"]) for item in aggregation]
 
 
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    aggregation = list(db.news.aggregate([
+        {"$unwind": "$categories"},
+        {"$group": {"_id": "$categories"}},
+        {"$sort": {"_id": 1}},
+        {"$limit": 5},
+        {"$project": {"_id": 0, "categories": "$_id"}}
+        ]))
+    return [item["categories"] for item in aggregation]
