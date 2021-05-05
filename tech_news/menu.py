4491ee3d0ec0895b5f26eb26c88ad2a1b5946b66
Sidnei Ramos <sidneiramos02@gmail.com>
2021-05-05 19:53:17 -0300

    win?

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -3,6 +3,13 @@ from tech_news.collector.importer import csv_importer
 from tech_news.collector.exporter import csv_exporter
 from tech_news.collector.scrapper import scrape, fetch_content
 from tech_news.database import create_news
+from tech_news.analyzer.search_engine import (
+    search_by_date,
+    search_by_category,
+    search_by_source,
+    search_by_title,
+)
+from tech_news.analyzer.ratings import top_5_categories, top_5_news
 
 
 def collector_menu():
@@ -34,5 +41,36 @@ def collector_menu():
         sys.stderr.write("Opção inválida\n")
 
 
+def choice_option(option):
+    if option == 1:
+        search_by_title(input())
+    elif option == 2:
+        search_by_date(input())
+    elif option == 3:
+        search_by_source(input())
+    elif option == 4:
+        search_by_category(input())
+
+
 def analyzer_menu():
     """Seu código deve vir aqui"""
+    print(
+        "Selecione uma das opções a seguir:\n"
+        " 1 - Buscar notícias por título;\n"
+        " 2 - Buscar notícias por data;\n"
+        " 3 - Buscar notícias por fonte;\n"
+        " 4 - Buscar notícias por categoria;\n"
+        " 5 - Listar top 5 notícias;\n"
+        " 6 - Listar top 5 categorias;\n"
+        " 7 - Sair."
+    )
+    option = int(input())
+    choice_option(option)
+    if option == 5:
+        top_5_news(input())
+    elif option == 6:
+        top_5_categories(input())
+    elif option == 7:
+        sys.stdout.write("Encerrando script\n")
+    else:
+        sys.stderr.write("Opção inválida\n")
