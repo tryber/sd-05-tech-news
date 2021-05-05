77592efb4955828c46da7ee0c296fe619ac3b41c
Lucas Yoshida <lucalsimayoshida@gmail.com>
2021-05-05 13:11:38 -0300

    organizando imports

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,12 +1,15 @@
 import sys
+from tech_news.database import create_news
 from tech_news.collector.importer import csv_importer
 from tech_news.collector.exporter import csv_exporter
 from tech_news.collector.scrapper import scrape, fetch_content
-from tech_news.database import create_news
-from tech_news.analyzer.search_engine import search_by_title, search_by_date
-from tech_news.analyzer.search_engine import search_by_source
-from tech_news.analyzer.search_engine import search_by_category
 from tech_news.analyzer.ratings import top_5_categories, top_5_news
+from tech_news.analyzer.search_engine import (
+    search_by_date,
+    search_by_category,
+    search_by_source,
+    search_by_title,
+)
 
 
 def collector_menu():
