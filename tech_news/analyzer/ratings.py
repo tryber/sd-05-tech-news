ca9d0d124b51a0392ccc748c64a9000887fbc091
luialbeto <araujo.script@gmail.com>
2021-05-04 22:28:12 -0300

    req 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from collections import Counter
 
 
 def top_5_news():
@@ -6,7 +7,7 @@ def top_5_news():
     result = sorted(
         news,
         key=lambda item: item["comments_count"] + item["shares_count"],
-        reverse=true,
+        reverse=True,
     )
     if len(result) == 0:
         return []
@@ -14,3 +15,9 @@ def top_5_news():
 
 
 def top_5_categories():
+    categories_list = search_news({})
+    result = [item["categories"][1] for item in categories_list]
+    top_five = Counter(result).most_common(5)
+    if len(result) == 0:
+        return []
+    return top_five
--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,6 +1,34 @@
-def collector_menu():
-    """Seu código deve vir aqui"""
+import sys
+from tech_news.collector.importer import csv_importer
+from tech_news.collector.exporter import csv_exporter
+from tech_news.collector.scrapper import scrape, fetch_content
+from tech_news.database import create_news
 
+
+def collector_menu():
+    print("Selecione uma das opções a seguir:")
+    print(" 1 - Importar notícias a partir de um arquivo CSV;")
+    print(" 2 - Exportar notícias para CSV;")
+    print(" 3 - Raspar notícias online;")
+    print(" 4 - Sair.")
+    options = input()
+    if options == "1":
+        print("Digite o nome do arquivo CSV a ser importado:")
+        csv_import = input()
+        create_news(csv_importer(csv_import))
+    elif options == "2":
+        print("Digite o nome do arquivo CSV a ser exportado:")
+        csv_export = input()
+        csv_exporter(csv_export)
+    elif options == "3":
+        print("Digite a quantidade de páginas a serem raspadas:")
+        pages_csv = input()
+        create_news(scrape(fetcher=fetch_content, pages=int(pages_csv)))
+
+    elif options == "4":
+        sys.stdout.write("Encerrando script\n")
+    else:
+        sys.stderr.write("Opção inválida\n") 
 
 def analyzer_menu():
     """Seu código deve vir aqui"""
