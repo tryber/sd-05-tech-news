ae88946e922db9bb4dd679fba6e1c827d1a978ef
rafaelpbar <rafael.coper@outlook.com>
2021-05-05 20:27:24 -0300

    By Zambs and Nat and Sid and Arau and Mitch - tks

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,23 @@
+from tech_news.database import search_news
+from collections import Counter
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    list_news = search_news({})
+    resultado = sorted(
+        list_news,
+        key=lambda item: item["comments_count"] + item["shares_count"],
+        reverse=True
+        )
+    if len(resultado) == 0:
+        return []
+    return [(item["title"], item["url"]) for item in resultado][:5]
 
 
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    list_category = search_news({})
+    resultado = [item["categories"][1] for item in list_category]
+    top_categories = Counter(resultado).most_common(5)
+    if len(resultado) == 0:
+        return []
+    return top_categories
