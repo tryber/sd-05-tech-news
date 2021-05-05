cd9812c3ee2c815a913bbc2c02fe3f2bfcbca5dc
Maurício <mauricio.sdjr@gmail.com>
2021-05-05 01:10:41 -0300

    requisitos 5,6,7 e 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,14 +1,44 @@
+from datetime import datetime
+from tech_news import database
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = database.search_news({"title": {"$regex": title, "$options": "-i"}})
+
+    if len(news) == 0:
+        return news
+
+    return [(news[0]["title"], news[0]["url"])]
 
 
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        news = database.search_news({"timestamp": {"$regex": date}})
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        if len(news) == 0:
+            return news
+    return [(news[0]["title"], news[0]["url"])]
 
 
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news = database.search_news(
+        {"sources": {"$regex": source, "$options": "-i"}}
+    )
+
+    if len(news) == 0:
+        return news
+
+    return [(news[0]["title"], news[0]["url"])]
 
 
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = database.search_news(
+        {"categories": {"$regex": category, "$options": "-i"}}
+    )
+
+    if len(news) == 0:
+        return news
+    return [(news[0]["title"], news[0]["url"])]
